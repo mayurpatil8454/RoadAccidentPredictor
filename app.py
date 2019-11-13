20ea3c8ea5a375d8cc2c98eecfed927d72819fb5
mayurpatil8454 <35427643+mayurpatil8454@users.noreply.github.com>
2019-11-13 18:46:53 +0530

    Update app.py

--- app.py
+++ app.py
@@ -145,4 +145,4 @@ def Predict(x):
 if __name__ == '__main__':
     # Bind to PORT if defined, otherwise default to 5000.
     port = int(os.environ.get('PORT', 8000))
-    app.run(host='0.0.0.0', port=port)
+    app.run(debug=True,host='0.0.0.0', port=port)
