4833ab5a1b78d9dfe64f7f5a531c73a9e7a4fc7a
mayurpatil8454 <35427643+mayurpatil8454@users.noreply.github.com>
2019-11-24 10:00:57 +0530

    'addLogo'

--- Index.py
+++ Index.py
@@ -1,40 +1,39 @@
-# import pandas as py
-# import matplotlib as mat
-# import  seaborn as sns
-# from sklearn.model_selection import train_test_split
-# from sklearn.linear_model import LogisticRegression
-# from sklearn.metrics import accuracy_score
-# import joblib as jobs
-# AccidentData = py.read_csv(r"C:\Users\MAYUR\PycharmProjects\Pythontutorials\Newdataset.csv")
-# y = AccidentData['City']
-# X = AccidentData.drop('City', axis=1)
+import pandas as py
+import matplotlib as mat
+import  seaborn as sns
+from sklearn.model_selection import train_test_split
+from sklearn.linear_model import LogisticRegression
+from sklearn.metrics import accuracy_score
+import joblib as jobs
+AccidentData = py.read_csv(r"C:\Users\MAYUR\PycharmProjects\Pythontutorials\Newdataset.csv")
+y = AccidentData['City']
+X = AccidentData.drop('City', axis=1)
+X_train, X_test, y_train, y_test = train_test_split(X, y, test_size=0.33, random_state=42)
+model=LogisticRegression();
+model.fit(X_train , y_train)
+pred = model.predict(X_test)
+print(accuracy_score(y_test,pred))
 #
-# X_train, X_test, y_train, y_test = train_test_split(X, y, test_size=0.33, random_state=42)
-# model=LogisticRegression();
-# model.fit(X_train , y_train)
-# pred = model.predict(X_test)
-# print(accuracy_score(y_test,pred))
-#
-# jobs.dump(model, 'RoadAccidentPredictor.pkl', compress=9)
-# model_clone = jobs.load('RoadAccidentPredictor.pkl')
-# print(type(X_test))
-# pred1 = model_clone.predict(X_test)
-# print(accuracy_score(y_test,pred1))
-# obj = {"VehicleType": [5],
-#        "Road_Type": [9],
-#        "Speed_limit":[50],
-#        "Light_Conditions":[7],
-#        "Weather_Conditions":[9],
-#        "Road_Surface_Conditions":[4],
-#        "Urban_or_Rural_Area":[1],
-#        "City":[24],
-#        "Sex":[0],
-#        "Age":[49]
-#        }
-# datapoint = py.DataFrame(obj)
-# predict = 7 if model_clone.predict(datapoint) >= 7 else model_clone.predict(datapoint)
-# print(predict)
-# print(predict * 10);
+jobs.dump(model, 'RoadAccidentPredictor.pkl', compress=9)
+model_clone = jobs.load('RoadAccidentPredictor.pkl')
+print(type(X_test))
+pred1 = model_clone.predict(X_test)
+print(accuracy_score(y_test,pred1))
+obj = {"VehicleType": [5],
+       "Road_Type": [9],
+       "Speed_limit":[50],
+       "Light_Conditions":[7],
+       "Weather_Conditions":[9],
+       "Road_Surface_Conditions":[4],
+       "Urban_or_Rural_Area":[1],
+       "City":[24],
+       "Sex":[0],
+       "Age":[49]
+       }
+datapoint = py.DataFrame(obj)
+predict = 7 if model_clone.predict(datapoint) >= 7 else model_clone.predict(datapoint)
+print(predict)
+int(predict * 10);
 #
 # sns.countplot(x='Speed_limit',data=AccidentData)
 # mat.pyplot.show()
--- app.py
+++ app.py
@@ -1,5 +1,4 @@
 from flask import Flask, render_template ,url_for,request,redirect,session,send_file
-import matplotlib as mat
 import urllib.parse
 import pandas as py
 import joblib as jobs
